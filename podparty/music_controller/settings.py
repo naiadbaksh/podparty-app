a3e2252b4a5079d05755a11d5f0d6e18c1d870dd
Naiad Baksh <naiadbaksh@gmail.com>
2022-01-21 23:32:52 -0500

    adding deployment features gunicorn whitenoise and others for heroku

--- podparty/music_controller/settings.py
+++ podparty/music_controller/settings.py
@@ -15,17 +15,21 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'whfxmu35o6%2)9z71og9*efq^7++so1%@i-nwekkj2d8&fi=$t'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["thoughtgarden-app.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
